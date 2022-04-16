12842fc24c8efe0d392555c7ae53c6fe498a562d
Jack Whelan <jackw97@live.ie>
2022-04-16 21:42:01 +0100

    Delete services/cvi-monitor directory
    A lot has changed, will push new flask based microservice to be reviewed by yourself @RuadhanMulcahy . This one's gotta go first.

--- services/cvi-monitor/tests/test_hello_world.py
+++
@@ -1,5 +0,0 @@
-from monitor_server.operators.hello_world import HelloWorld
-
-def test_hello_world():
-    hello_world_operator = HelloWorld()
-    assert hello_world_operator.say_hello() == 'Hello World!'
