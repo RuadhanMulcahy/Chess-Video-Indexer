03ee4ffa8be203d277b7c0e84e9fb7c3bdd188be
Ruadhan Mulcahy <ruadhanmulcahy@outlook.com>
2022-06-15 23:03:31 +0100

    Work in progress - Added Reverse Move Detection, GameHandler

--- services/video_chess_indexer/game.py
+++ services/video_chess_indexer/game.py
@@ -1,98 +1,29 @@
-from board import Board
-from move import Move
-
 class Game:
     def __init__(self):
-        self.create_game_starts()
-        self.last_valid_board = Board()
-        self.move_count = 0
-        self.same_count = 0   
-        self.prev_valid_time_stamp = 0
         self.moves = []
-        self.games = []
-        self.game = []
-        self.flipped = None
+        self.moves_ahead = []
+        self.move_count = 0
 
-    def read_position(self, board, time_stamp):
-        self.check_for_game_start(board)
+    def add_move(self, move):
+        self.moves.append(move)
+        if self.reverse_move_check():
+            self.moves.pop()
+            self.moves_ahead.append(move)
 
-        if self.prev_valid_time_stamp is None:
-            self.prev_valid_time_stamp = time_stamp
-
-        if self.move_count < 10:
-            if self.valid_highlight_squares(board): 
-                if board.compare(self.last_valid_board, True) == False and self.same_count >= 10:    
-                    # self.last_valid_board.show_highlighted()
-                    move = self.get_move(self.last_valid_board, self.prev_valid_time_stamp)
-                    # move.show()
-                    self.moves.append(move)
-                    self.game.append(move)
-                    if self.reverse_move_check():
-                        self.game.pop()
-                        print(f"Reverse move {self.move_count}")
-                    self.move_count+=1
-                    self.same_count = 0
-                    self.prev_valid_time_stamp = time_stamp
-                self.last_valid_board = board
-                self.same_count += 1
-            return True     
-        for move in self.game:
+    def show_game(self):
+        print("Moves")
+        for move in self.moves:
             move.show()
-        return False
 
-    def get_move(self, board, time_stamp):
-        move = Move()
-        move.time_stamp = time_stamp
-        move.board = board
-        for row in board.squares:
-            for square in row:
-                if square.highlighted and square.piece_short != '':
-                    move.new_square = square
-                    move.new_square.set_pgn(self.flipped)
-                    move.color = square.color
-                elif square.highlighted and square.piece_short == '':
-                    move.prev_square = square
-                    move.prev_square.set_pgn(self.flipped)
-        return move
+        print("Moves ahead")
+        for move in self.moves_ahead:
+            move.show()
 
     def reverse_move_check(self):
-        if self.move_count <= 1:
+        if len(self.moves) < 2:
             return False
-        prev_move = self.moves[self.move_count - 2]
-        curr_move = self.moves[self.move_count -  1]
+        prev_move = self.moves[-2]
+        curr_move = self.moves[-1]
         if curr_move.board.squares[prev_move.prev_square.y][prev_move.prev_square.x].piece_short == prev_move.new_square.piece_short:
             return True
         return False
-        
-    def valid_highlight_squares(self, board):
-        highlighted_piece_count = 0
-        highlighted_empty_count = 0
-        for row in board.squares:
-            for square in row:
-                if square.highlighted == True and square.piece_short != '':
-                    highlighted_piece_count += 1
-                elif square.highlighted == True and square.piece_short == '':
-                    highlighted_empty_count += 1
-                if highlighted_piece_count > 1 or highlighted_empty_count > 1:
-                    return False
-        if highlighted_empty_count == 1 and highlighted_piece_count == 1:
-            return True
-        return False
-
-    def check_for_highlight_change(self):
-        if self.board.compare(self.last_valid_board, True) == False:
-            return True
-        return False
-
-    def create_game_starts(self):
-        self.board_normal = Board()
-        self.board_flipped = Board()
-
-        self.board_normal.create_game_start(False)
-        self.board_flipped.create_game_start(True)
-
-    def check_for_game_start(self, board):
-        if board.compare(self.board_normal, False):
-            self.flipped = False
-        elif board.compare(self.board_flipped, False):
-            self.flipped = True
--- services/video_chess_indexer/main.py
+++ services/video_chess_indexer/main.py
@@ -7,7 +7,7 @@ from yolov5.detect import run
 
 from file_handler import read_label_file, remove_old_label_file
 from convert import Convert
-from game import Game
+from game_handler import GameHandler
 
 video_name = '0'
 
@@ -19,22 +19,20 @@ def extract_integer(filename):
     return int(filename.split('_')[1].split('.')[0])
 
 file_names = sorted(os.listdir('./files/results/result/labels'), key=extract_integer)
-game = Game()
+game_handler = GameHandler()
 
 for file_name in file_names:
     convert = Convert(read_label_file(f"./files/results/result/labels/{file_name}"))
     board = convert.go()
-    if board is not None:
+    if board:
         # board.show_highlighted()
         # board.show()
         # time.sleep(1)
         # print(file_name)
         seconds = extract_integer(file_name) / 30
         # print(str(datetime.timedelta(seconds=seconds)))
-        if game.read_position(board, datetime.timedelta(seconds=seconds)) is False:
+        if game_handler.read_position(board, datetime.timedelta(seconds=seconds)) is False:
             break
-
-game.reverse_move_check()
 
 # # print("NOT IN SCOPE")
 # # print(i)
